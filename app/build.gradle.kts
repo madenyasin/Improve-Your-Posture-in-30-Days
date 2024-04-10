e5cbb20db817e888b53c653c533f87d80ab68f5b
Yasin Maden <maden.yasin@hotmail.com>
2024-04-11 01:50:33 +0300

    Add android-youtube-player dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,7 @@ android {
 
 dependencies {
 
+    implementation ("com.pierfrancescosoffritti.androidyoutubeplayer:core:12.1.0")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
