e4d984fe1d55fe1fccaa62c836aa41efc777bd98
Yasin Maden <maden.yasin@hotmail.com>
2024-04-11 02:39:52 +0300

    Refactor YoutubePlayer Composable function with modifier parameter

--- app/src/main/java/com/yasinmaden/improveyourposturein30days/ui/CustomComposableFuncs.kt
+++ app/src/main/java/com/yasinmaden/improveyourposturein30days/ui/CustomComposableFuncs.kt
@@ -1,18 +1,27 @@
 package com.yasinmaden.improveyourposturein30days.ui
 
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.lifecycle.LifecycleOwner
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView
 
+
 @Composable
 fun YoutubePlayer(
     youtubeVideoId: String,
-    lifecycleOwner: LifecycleOwner
+    lifecycleOwner: LifecycleOwner,
+    modifier: Modifier = Modifier
 ) {
-    AndroidView(
+    AndroidView(modifier = modifier.fillMaxWidth().padding(8.dp).clip(RoundedCornerShape(8.dp)),
         factory = { context ->
             YouTubePlayerView(context = context).apply {
                 lifecycleOwner.lifecycle.addObserver(this)
@@ -25,3 +34,4 @@ fun YoutubePlayer(
             }
         })
 }
+
