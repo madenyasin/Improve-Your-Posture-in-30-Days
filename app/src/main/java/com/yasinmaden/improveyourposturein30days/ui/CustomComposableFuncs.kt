0d79157454edbac7f88a40bbbdd03a73b397d85d
Yasin Maden <maden.yasin@hotmail.com>
2024-04-11 02:31:20 +0300

    Completed YoutubePlayer Composable function

---
+++ app/src/main/java/com/yasinmaden/improveyourposturein30days/ui/CustomComposableFuncs.kt
@@ -0,0 +1,27 @@
+package com.yasinmaden.improveyourposturein30days.ui
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.lifecycle.LifecycleOwner
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView
+
+@Composable
+fun YoutubePlayer(
+    youtubeVideoId: String,
+    lifecycleOwner: LifecycleOwner
+) {
+    AndroidView(
+        factory = { context ->
+            YouTubePlayerView(context = context).apply {
+                lifecycleOwner.lifecycle.addObserver(this)
+
+                addYouTubePlayerListener(object : AbstractYouTubePlayerListener() {
+                    override fun onReady(youTubePlayer: YouTubePlayer) {
+                        youTubePlayer.loadVideo(youtubeVideoId, 0f)
+                    }
+                })
+            }
+        })
+}
