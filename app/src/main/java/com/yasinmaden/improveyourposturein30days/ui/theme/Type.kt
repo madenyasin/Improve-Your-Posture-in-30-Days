7966f48d821cf60b73f8b7c1b9348df3ecf334a7
Yasin Maden <maden.yasin@hotmail.com>
2024-04-11 03:07:37 +0300

    Completed Material Theme changes

--- app/src/main/java/com/yasinmaden/improveyourposturein30days/ui/theme/Type.kt
+++ app/src/main/java/com/yasinmaden/improveyourposturein30days/ui/theme/Type.kt
@@ -2,33 +2,37 @@ package com.yasinmaden.improveyourposturein30days.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.yasinmaden.improveyourposturein30days.R
 
-// Set of Material typography styles to start with
+
+val AnekTamil = FontFamily(
+    Font(R.font.anek_tamil_regular),
+    Font(R.font.anek_tamil_bold, FontWeight.Bold)
+)
+
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+    displayLarge = TextStyle(
+        fontFamily = AnekTamil,
         fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
+        fontSize = 36.sp
+    ),
+    displayMedium = TextStyle(
+        fontFamily = AnekTamil,
+        fontWeight = FontWeight.Bold,
+        fontSize = 20.sp
     ),
     labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+        fontFamily = AnekTamil,
+        fontWeight = FontWeight.Bold,
+        fontSize = 14.sp
+    ),
+    bodyLarge = TextStyle(
+        fontFamily = AnekTamil,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp
     )
-    */
 )
