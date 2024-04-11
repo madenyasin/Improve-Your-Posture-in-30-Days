809f7988cc54252c794241a3d371c3ab7ceebe73
Yasin Maden <maden.yasin@hotmail.com>
2024-04-11 04:20:21 +0300

    refactor Composables

--- app/src/main/java/com/yasinmaden/improveyourposturein30days/ui/CustomComposableFuncs.kt
+++ app/src/main/java/com/yasinmaden/improveyourposturein30days/ui/CustomComposableFuncs.kt
@@ -1,17 +1,24 @@
 package com.yasinmaden.improveyourposturein30days.ui
 
+import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
@@ -20,11 +27,12 @@ import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView
 import com.yasinmaden.improveyourposturein30days.R
+import com.yasinmaden.improveyourposturein30days.data.DataSource
 import com.yasinmaden.improveyourposturein30days.model.Exercise
 import com.yasinmaden.improveyourposturein30days.ui.theme.ImproveYourPostureIn30DaysTheme
 import com.yasinmaden.improveyourposturein30days.ui.theme.Shapes
 
-
+// TODO: autoplay will be stopped.
 @Composable
 fun YoutubePlayer(
     youtubeVideoId: String,
@@ -33,8 +41,8 @@ fun YoutubePlayer(
 ) {
     AndroidView(modifier = modifier
         .fillMaxWidth()
-        .padding(8.dp)
-        .clip(Shapes.small),
+        .padding(dimensionResource(id = R.dimen.small))
+        .clip(Shapes.medium),
         factory = { context ->
             YouTubePlayerView(context = context).apply {
                 lifecycleOwner.lifecycle.addObserver(this)
@@ -51,33 +59,74 @@ fun YoutubePlayer(
 @Composable
 fun ExerciseCard(
     exercise: Exercise,
-    modifier: Modifier = Modifier){
-    Card {
-        Column {
-            Text(text = stringResource(exercise.nameRes))
-            YoutubePlayer(youtubeVideoId = stringResource(exercise.videoUrlRes), lifecycleOwner = LocalLifecycleOwner.current)
-            Text(text = stringResource(id = exercise.descriptionRes))
+    modifier: Modifier = Modifier
+) {
+    Card(
+        shape = Shapes.large,
+        elevation = CardDefaults.cardElevation(
+            defaultElevation = 2.dp
+        ), modifier = Modifier.padding(horizontal = dimensionResource(id = R.dimen.medium), vertical = dimensionResource(id = R.dimen.small))
+    ) {
+        Column(modifier = Modifier.padding(dimensionResource(id = R.dimen.medium))) {
+            Text(
+                text = stringResource(exercise.nameRes),
+                style = MaterialTheme.typography.titleLarge,
+                modifier = Modifier.padding(dimensionResource(id = R.dimen.small))
+            )
+            YoutubePlayer(
+                youtubeVideoId = stringResource(exercise.videoUrlRes),
+                lifecycleOwner = LocalLifecycleOwner.current
+            )
+            Text(
+                text = stringResource(id = exercise.descriptionRes),
+                style = MaterialTheme.typography.bodyLarge,
+                modifier = Modifier.padding(dimensionResource(id = R.dimen.small))
+            )
         }
     }
 }
 
-@Preview(showBackground = true)
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun ExerciseCardPreview(){
-    ImproveYourPostureIn30DaysTheme{
-        val exercise1 = Exercise(
-            nameRes = R.string.day1_title,
-            videoUrlRes = R.string.day1_videoURL,
-            descriptionRes = R.string.day1_description
-        )
-        ExerciseCard(exercise1)
+fun ExerciseApp(modifier: Modifier = Modifier) {
+    Scaffold(topBar = {
+        CenterAlignedTopAppBar(title = {
+            Text(
+                text = "Improve Your Posture",
+                style = MaterialTheme.typography.titleLarge
+            )
+        })
+    }) { paddingValues ->
+        LazyColumn(modifier = modifier.padding(paddingValues)) {
+            items(DataSource.exercises) { exercise ->
+                ExerciseCard(exercise = exercise)
+            }
+        }
     }
 
 }
+
 @Preview(showBackground = true)
 @Composable
-fun AppPreview(){
-    ImproveYourPostureIn30DaysTheme{
+fun ExerciseCardPreview() {
+    ImproveYourPostureIn30DaysTheme {
+//        val exercise1 = Exercise(
+//            nameRes = R.string.day1_title,
+//            videoUrlRes = R.string.day1_videoURL,
+//            descriptionRes = R.string.day1_description
+//        )
+//        ExerciseCard(exercise1)
 
+
+    }
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun AppPreview() {
+    ImproveYourPostureIn30DaysTheme {
+        ExerciseApp()
     }
 }
