56051cb0477afd3b09100859dd26b923641e61a4
Yasin Maden <maden.yasin@hotmail.com>
2024-04-11 03:46:21 +0300

    Rename Exercises.kt to Exercise.kt

--- app/src/main/java/com/yasinmaden/improveyourposturein30days/model/Exercise.kt
+++ app/src/main/java/com/yasinmaden/improveyourposturein30days/model/Exercise.kt
@@ -2,7 +2,7 @@ package com.yasinmaden.improveyourposturein30days.model
 
 import androidx.annotation.StringRes
 
-data class Exercises(
+data class Exercise(
     @StringRes val nameRes: Int,
     @StringRes val descriptionRes: Int,
     @StringRes val videoUrlRes: Int
